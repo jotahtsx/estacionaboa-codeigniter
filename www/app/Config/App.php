40adcac920ce77212ef4185ab5efbe6245973626
João M S Silva <joaomanoelskai@gmail.com>
2025-04-07 21:21:15 -0300

    Configurando theme do BS

--- www/app/Config/App.php
+++ www/app/Config/App.php
@@ -4,6 +4,7 @@ namespace Config;
 
 use CodeIgniter\Config\BaseConfig;
 
+
 class App extends BaseConfig
 {
     /**
@@ -16,7 +17,7 @@ class App extends BaseConfig
      *
      * E.g., http://example.com/
      */
-    public string $baseURL = 'http://localhost:8080/';
+    public string $baseURL = 'http://localhost:4500/';
 
     /**
      * Allowed Hostnames in the Site URL other than the hostname in the baseURL.
