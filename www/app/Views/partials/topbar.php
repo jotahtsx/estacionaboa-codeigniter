62e713a4ea0e89004225943eeb2f9b32887b57c5
João M S Silva <joaomanoelskai@gmail.com>
2025-04-12 19:16:32 -0300

    feat(auth): login e logout com CI Shield funcionando

--- www/app/Views/partials/topbar.php
+++ www/app/Views/partials/topbar.php
@@ -9,8 +9,10 @@
             <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
                 <li><a class="dropdown-item" href="#">Settings</a></li>
                 <li><a class="dropdown-item" href="#">Activity Log</a></li>
-                <li><hr class="dropdown-divider" /></li>
-                <li><a class="dropdown-item" href="#">Logout</a></li>
+                <li>
+                    <hr class="dropdown-divider" />
+                </li>
+                <li><a class="dropdown-item" href="<?= url_to('logout') ?>">Sair</a></li>
             </ul>
         </li>
     </ul>
