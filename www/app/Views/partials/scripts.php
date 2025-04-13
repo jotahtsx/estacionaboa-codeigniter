f77160fee9117806bf353797ef6cca73b78ae21b
João M S Silva <joaomanoelskai@gmail.com>
2025-04-13 20:52:27 -0300

    configurando dinamicamente a classe status da sidebar de users

--- www/app/Views/partials/scripts.php
+++ www/app/Views/partials/scripts.php
@@ -1,5 +1,43 @@
 <script src="<?= base_url('libs/bootstrap@5.2.3/bootstrap.bundle.min.js') ?>"></script>
 <script src="<?= base_url('js/app.js') ?>"></script>
 <script src="<?= base_url('libs/simple-datatables@7.1.2/simple-datatables.min.js') ?>"></script>
+<script>
+    window.addEventListener('DOMContentLoaded', event => {
+        document.querySelectorAll('.datatable').forEach(table => {
+            new simpleDatatables.DataTable(table, {
+                labels: {
+                    placeholder: "Buscar...",
+                    perPage: "Registros por página",
+                    noRows: "Nenhum registro disponível",
+                    noResults: "Nenhum resultado corresponde à sua busca",
+                    info: "Mostrando de {start} a {end} de {rows} registros",
+                    loading: "Carregando...",
+                    infoFiltered: " – filtrado de {rows} registros no total"
+                },
+                layout: {
+                    top: "{search}",
+                    bottom: `
+                        <div class="datatable-bottom">
+                            <div class="datatable-dropdown">{select}</div> <!-- Seletor de quantidade -->
+                            <div class="datatable-info">{info}</div> <!-- Info de quantos registros -->
+                            <div class="datatable-pagination">{pager}</div> <!-- Controles de página -->
+                        </div>
+                    `
+                },
+                pagination: {
+                    previous: "Anterior",
+                    next: "Próximo",
+                    navigate: "Ir para a página",
+                    page: "Página {page}",
+                    showing: "Mostrando página {page} de {pages}",
+                    of: "de"
+                }
+            });
+        });
+    });
+</script>
+
+
 </body>
+
 </html>
--- www/app/Views/users/index.php
+++ www/app/Views/users/index.php
@@ -11,6 +11,40 @@
                     Listagem e gerenciamento dos usuários do sistema.
                 </li>
             </ol>
+            <div class="card mb-4">
+                <div class="card-body">
+                    <table class="datatable">
+                        <thead>
+                            <tr>
+                                <th>ID</th>
+                                <th>Username</th>
+                                <th>Email</th>
+                                <th>Criado em</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <?php if (!empty($users)) : ?>
+                                <?php foreach ($users as $user) : ?>
+                                    <tr>
+                                        <td><?= $user->id ?></td>
+                                        <td><?= $user->username ?></td>
+                                        <td><?= $user->email ?></td>
+                                        <td><?= $user->created_at ?></td> </tr>
+                                    </tr>
+                                <?php endforeach; ?>
+                            <?php endif; ?>
+                        </tbody>
+                        <tfoot>
+                            <tr>
+                                <th>ID</th>
+                                <th>Username</th>
+                                <th>Email</th>
+                                <th>Criado em</th>
+                            </tr>
+                        </tfoot>
+                    </table>
+                </div>
+            </div>
         </div>
     </main>
 </div>
