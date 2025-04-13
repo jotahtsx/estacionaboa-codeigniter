9f77a912d9044045ba14c0b3c3f7b07e1978346f
João M S Silva <joaomanoelskai@gmail.com>
2025-04-12 21:04:06 -0300

    ajustando header e styles

--- www/app/Views/partials/topbar.php
+++ www/app/Views/partials/topbar.php
@@ -1,6 +1,8 @@
 <nav class="sb-topnav navbar navbar-expand navbar-dark bg-dark">
     <a class="navbar-brand ps-3" href="<?= base_url() ?>">Estacionaboa</a>
-    <button class="btn btn-link btn-sm order-1 order-lg-0 me-4 me-lg-0" id="sidebarToggle"><i class="fas fa-bars"></i></button>
+    <button class="btn btn-link btn-sm order-1 order-lg-0 me-4 me-lg-0" id="sidebarToggle">
+        <i class="fas fa-bars"></i>
+    </button>
     <ul class="navbar-nav ms-auto me-0 me-md-3 my-2 my-md-0 me-lg-4">
         <?php
         $user = auth()->user();
@@ -9,7 +11,7 @@
         ?>
         <li class="nav-item dropdown">
             <a class="nav-link dropdown-toggle d-flex align-items-center" id="navbarDropdown" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                <img src="<?= $imagePath ?>" alt="<?= esc($altText) ?>" title="<?= esc($altText) ?>" class="rounded-circle" style="width: 32px; height: 32px; object-fit: cover;">
+                <img src="<?= $imagePath ?>" alt="<?= esc($altText) ?>" title="<?= esc($altText) ?>" class="rounded-circle" style="width: 35px; height: 35px; object-fit: cover; border: 2px solid #fff; transition: transform 0.3s ease;">
             </a>
             <ul class="dropdown-menu dropdown-menu-end p-0 m-0" aria-labelledby="navbarDropdown">
                 <li>
