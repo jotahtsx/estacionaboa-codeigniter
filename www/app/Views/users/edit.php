19c73a930d4d2f634e673fc17f302cb18c09b6e7
João M S Silva <joaomanoelskai@gmail.com>
2025-05-06 06:26:30 -0300

    atualizando views

--- www/app/Views/users/edit.php
+++ www/app/Views/users/edit.php
@@ -4,8 +4,6 @@
 
 <div id="layoutSidenav_content">
     <main>
-
-
         <div class="container-fluid px-4">
             <h1 class="mt-4">Editar: <b><?= esc($user->first_name) ?>  <?= esc($user->last_name) ?></b></h1>
             <ol class="breadcrumb mb-4">
