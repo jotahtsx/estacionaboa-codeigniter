a10ab4890fd3f98e0b9610f28859a13eed915492
João M S Silva <joaomanoelskai@gmail.com>
2025-06-25 17:50:17 -0300

    Inicializando configurações, adicionando rotas de configurações

--- www/app/Config/Routes.php
+++ www/app/Config/Routes.php
@@ -17,6 +17,8 @@ $routes->get('/logout', 'Admin\AuthController::logout', ['as' => 'logout']);
 
 // Grupo de Rotas para Administração
 $routes->group('admin', ['filter' => 'session', 'namespace' => 'App\Controllers\Admin'], function ($routes) {
+    
+    //Rotas de usuários
     $routes->get('usuarios', 'UserController::index', ['as' => 'admin_usuarios']);
     $routes->get('usuarios/cadastrar', 'UserController::create', ['as' => 'admin_usuarios_create']);
     $routes->post('usuarios/store', 'UserController::store', ['as' => 'admin_usuarios_store']);
@@ -24,4 +26,8 @@ $routes->group('admin', ['filter' => 'session', 'namespace' => 'App\Controllers\
     $routes->get('usuarios/editar/(:num)', 'UserController::edit/$1', ['as' => 'admin_usuarios_edit']);
     $routes->put('usuarios/atualizar/(:num)', 'UserController::update/$1', ['as' => 'admin_usuarios_update']);
     $routes->delete('usuarios/delete/(:num)', 'UserController::delete/$1', ['as' => 'admin_usuarios_delete']);
+
+    //Rotas de configurações
+    $routes->get('configuracoes', 'ParkingSettingController::edit');
+    $routes->post('configuracoes', 'ParkingSettingController::update');
 });
