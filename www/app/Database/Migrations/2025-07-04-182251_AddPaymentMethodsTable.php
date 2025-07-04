5e72ab49792b42f3ef23d792eeb0961d96db4a86
João M S Silva <joaomanoelskai@gmail.com>
2025-07-04 15:30:57 -0300

    Criando migrate de formas de pagamentos

---
+++ www/app/Database/Migrations/2025-07-04-182251_AddPaymentMethodsTable.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class AddPaymentMethodsTable extends Migration
+{
+    public function up()
+    {
+        $this->forge->addField([
+            'id' => [
+                'type'           => 'INT',
+                'constraint'     => 11,
+                'unsigned'       => true,
+                'auto_increment' => true,
+            ],
+            'name' => [
+                'type'       => 'VARCHAR',
+                'constraint' => '100',
+                'unique'     => true,
+            ],
+            'description' => [
+                'type'       => 'VARCHAR',
+                'constraint' => '255',
+                'null'       => true,
+            ],
+            'active' => [
+                'type'       => 'TINYINT',
+                'constraint' => 1,
+                'default'    => 1, // Padrão para ativo
+            ],
+            'created_at datetime default current_timestamp',
+            'updated_at datetime default current_timestamp on update current_timestamp',
+            'deleted_at datetime default null',
+        ]);
+
+        $this->forge->addPrimaryKey('id');
+        $this->forge->createTable('payment_methods');
+    }
+
+    public function down()
+    {
+        $this->forge->dropTable('payment_methods');
+    }
+}
