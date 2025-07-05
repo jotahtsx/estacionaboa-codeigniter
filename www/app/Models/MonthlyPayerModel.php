a0add71e26c73e9a01a095f4288c91221a63975d
João M S Silva <joaomanoelskai@gmail.com>
2025-07-05 08:04:10 -0300

    Criando migrate e models de Mensalistas

---
+++ www/app/Models/MonthlyPayerModel.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+class MonthlyPayerModel extends Model
+{
+    protected $table            = 'monthly_payers';
+    protected $primaryKey       = 'id';
+    protected $useAutoIncrement = true;
+    protected $returnType       = 'array';
+
+    protected $allowedFields = [
+        'first_name',
+        'last_name',
+        'birth_date',
+        'cpf',
+        'rg',
+        'email',
+        'phone',
+        'zip_code',
+        'street',
+        'number',
+        'neighborhood',
+        'city',
+        'state',
+        'complement',
+        'active',
+        'due_day',
+        'notes',
+    ];
+    protected $useTimestamps = true;
+    protected $createdField  = 'created_at';
+    protected $updatedField  = 'updated_at';
+}
